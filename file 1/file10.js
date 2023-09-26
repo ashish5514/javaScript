95a1378a970b4b9d109ec10ff375ee99fe9709e6
ashish5514 <zankatashish680@gmail.com>
2023-09-26 11:54:45 +0530

    js

---
+++ file 1/file10.js
@@ -0,0 +1,25 @@
+   //truthy and falsy value
+
+   // falsy value
+   
+//    false
+//    ""
+//    null
+//    undefined
+//    0
+
+
+// truthy maa aa value aave to j true thai 
+//baki upar ni badhi false aave
+
+//"abc"
+// 1, -1
+
+
+ let firstName=0;
+
+ if(firstName){
+    console.log(firstName);
+ }else{
+    console.log("firstName is empty");
+ }
