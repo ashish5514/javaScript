58068cbfbceda76f5e883ff4ff0f612715e0c322
ashish5514 <zankatashish680@gmail.com>
2023-09-28 18:14:12 +0530

    fuction

---
+++ file 3/function01.js
@@ -0,0 +1,75 @@
+function singasong (){
+    console.log("happy birthday to you......");
+}
+
+//  function toeplusfour(){
+//     console.log(2+4);
+//  }
+
+//  toeplusfour();
+
+// function twoplusfour (){
+//     return 2+4;
+// }
+// const returnvalue = twoplusfour();
+// console.log(returnvalue);
+
+// function sumtwonumber (number1,number2){
+//     return number1+number2;
+// }
+
+// const returnvalue = sumtwonumber(4,5);
+// console.log(returnvalue);
+
+
+  // od or even
+   // input: 1 nunber
+   // output : true & false
+   
+//    function iseven(number){
+//     if(number % 2 === 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+//    }
+//      console.log(  iseven(5));
+   
+//    function iseven(number){
+//       return number  % 2 === 0;
+//    }
+//      console.log(  iseven(3));
+
+
+  //  function
+//   input: String
+//   output :  firstcharacter
+
+
+//   function firstcharacter(anystring){
+//     return anystring [0];
+
+//   }
+//   console.log(firstcharacter("abc"));
+
+
+ //function 
+//  input: Array,target (number)
+// output:index of target if target  present in Array
+// [1,5,9,27,6],9
+
+function findTarget(array, target){
+    for(let i = 0;i<array.lengh; i++){
+        if(array[i]===target){
+            return i;
+        }
+    }
+       return -1;
+}
+ 
+    const myarray =[1,3,8,90];
+    const ans =findTarget(myarray,8)
+    console.log(ans);
+
+
+
