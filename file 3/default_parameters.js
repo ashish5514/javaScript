0f58b26f3234103d118c1619c47eeb57f2614aaf
ashish5514 <zankatashish680@gmail.com>
2023-10-09 13:43:17 +0530

    js

--- file 3/default_parameters.js
+++ file 3/default_parameters.js
@@ -1,3 +1,16 @@
   // default parameters
 
+  // function addtwo (a,b=0){
+  //   if(typeof b ==="undefined"){
+  //     b=1;
+  //   }
+  //   return a+b ;
+  // }.
+
+
+  function addtwo (a,b){
+    return a+b ; 
   
+    }
+  const ans = addtwo(4,8);
+  console.log(ans);
