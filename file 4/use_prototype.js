894070130c905fc70a9eb83a9d81b95feac3544a
ashish5514 <zankatashish680@gmail.com>
2023-10-13 12:52:13 +0530

    update

--- file 4/use_prototype.js
+++ file 4/use_prototype.js
@@ -1,6 +1,8 @@
-  function hello(){
-    console.log("hello world");
-  }
+  // function hello(){
+  //   console.log("hello world");
+  // }
+
+  //  const hello = {key1: "value1"}
 
   // javascript function ===> function + object 
 
@@ -11,9 +13,74 @@
   // console.log(hello.myOwnProperty);
 
   // name property ---> tells function name ;
-
+ 
   // function provides more usefull properties.
 
 //   hello.prototype;
 
 // only functions provide prototype propety
+
+  //  if(hello.prototype){
+  //   console.log("prototype is present");
+  //  }else{
+  //   console.log("prototype is not present");
+  //  }
+
+  // hello.prototype.abc = "abc";
+  // hello.prototype.xyz = "xyz";
+  // hello.prototype.sing = function(){
+  //   return "lallalal";
+  // };
+  // console.log(hello.prototype.sing());
+
+  const userMethod = {
+    about: function (){
+        return `${this.firstName} is ${this.age} years old`
+        },
+        is18: function (){
+                    return this.age >= 18;
+        },
+        sing: function(){
+            return'toon na na na la la';
+        }
+        
+}
+
+
+  function creatUser (firstName, lastName, email, age, address){
+    
+    const user = Object.create(userMethod);
+    user.firstName=firstName;
+    user.lastName=lastName;
+    user.email=email;
+    user.age=age;
+    user.address=address;
+    
+    return user;
+    
+}
+
+    creatUser.prototype.about = function (){
+      return`${this.firstName}is ${this.age}years old`
+    };
+    creatUser.prototype.is18 = function (){
+      return this.age>=18;
+
+    }
+    creatUser.prototype.sing = function(){
+      return"la la la";
+    }
+     
+  
+ console.log(creatUser.prototype);
+ const user1 = creatUser('ashish','sam','ashish1@gmail.com',19,"my address");
+ const user2 = creatUser('khushvant','sam','khushvant2@gmail.com',26,"my address");
+ const user3 = creatUser('trun','sam','tarun3@gmail.com',24,"my address");
+
+ console.log(user1);
+ console.log(user1.is18());
+//  console.log(user2.about());
+//  console.log(user3.about());
+
+
+   
