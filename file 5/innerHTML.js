c9f75d637f4bb9da890f795127fb19aa67aaeaec
ashish5514 <zankatashish680@gmail.com>
2023-10-19 12:52:06 +0530

    update

---
+++ file 5/innerHTML.js
@@ -0,0 +1,7 @@
+  //inner HTML
+    const headline = document.querySelector(".headline");
+    // console.log(headline.innerHTML);
+    headline.innerHTML = "<h1>Inner HTML changed </h1>";
+    headline.innerHTML = "<button class =\"btn\">Learn More</button>";
+
+
