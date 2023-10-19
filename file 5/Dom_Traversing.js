ec0bf2434d208f4ad58f982bb246a541157d129b
ashish5514 <zankatashish680@gmail.com>
2023-10-19 17:46:53 +0530

    update

---
+++ file 5/Dom_Traversing.js
@@ -0,0 +1,27 @@
+// const rootnode = document.getRootNode();
+
+// // console.log(htmlElementNode.childNodes); // NodeList(3) [head, text, body]
+// const headElementNode = htmlElementNode.childNodes[0];
+// const textNOde1 = htmlElementNode.childNodes[1];
+// const bodyElemenyNode = htmlElementNode.childNodes[2];
+// console.log(headElementNode.childNodes);
+//   sibling relation 
+
+//  const h1 = document.querySelector("h1");
+//  const body = h1.parentNode.parentNode;
+//  body.style.color ="#efefef"
+//  body.style.backgroundColor ="#333"
+
+// const body = document.body;
+// body.style.color ="#efefef"
+// body.style.backgroundColor ="#323"
+
+// const head  = document.querySelector("head");
+// console.log(head);
+// const title = head.querySelector("title");
+// console.log(title);
+// console.log(title.childNodes);
+
+
+const container = document.querySelector(".container");
+ console.log(container.children);
---
+++ file 5/classList_ADD_Remove_Toggle.js
@@ -0,0 +1,30 @@
+  //classlist add remove class
+  // toggle class
+
+
+  const sectiontodo = document.querySelector(".section-todo");
+//   console.log(sectiontodo.classList);
+
+            // add classs
+
+
+//   sectiontodo.classList.add('bg-dark');
+
+
+            // remove class
+
+
+    //  sectiontodo.classList.remove("container");
+    
+    // const ans =  sectiontodo.classList.contains("container");
+    // console.log(ans);
+    
+    //toggle class
+    
+    //  sectiontodo.classList.toggle("bg-dark");
+    //  sectiontodo.classList.toggle("bg-dark");
+
+
+    //  const header = document.querySelector(".header");
+    //  console.log(header.classList);
+    //  header.classList.add("bg-dark");
