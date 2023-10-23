68fe4576faae57e2b7c8ae79d195d80b09c4d12b
ashish5514 <zankatashish680@gmail.com>
2023-10-23 13:01:34 +0530

    update

---
+++ file 6/Event_object.js
@@ -0,0 +1,9 @@
+ // event object
+
+ const firstButton = document.querySelector("#btn-1");
+ firstButton.addEventListener("click", function(){
+    console.log(this);
+ })
+
+ // jab bhi me kisi bhi element me event listener add hoga
+ // 
