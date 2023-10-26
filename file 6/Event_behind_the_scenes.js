856d01069bc320c705a783b456c5125fa0f30ae6
ashish5514 <zankatashish680@gmail.com>
2023-10-26 12:55:09 +0530

    update

--- file 6/Event_behind_the_scenes.js
+++ file 6/Event_behind_the_scenes.js
@@ -4,10 +4,18 @@ const allButtons = document.querySelectorAll(".my-buttons button");
 
 allButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.currentTarget.textContent);
+    let num = 0 ;
+    for (let i= 0; i<= 1000000000; i++){
+      num+= i;
+    }
+    console.log(e.currentTarget.textContent, num);
   });
 });
 
 
-
-console.log("script start !!!!!!");
+  let outerVar = 0;
+  for(let i = 0; i<= 1000000000; i++){
+    outerVar+= i;
+  }
+  console.log("value of outervar ", outerVar);
+console.log("script end !!!!!!");
