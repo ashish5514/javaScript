0bac4843ed63b6af8a6e421dc2413f77cc61f81b
ashish5514 <zankatashish680@gmail.com>
2023-11-03 17:10:55 +0530

    update

--- file 10/Callback_hell_to_flat_code.js
+++ file 10/Callback_hell_to_flat_code.js
@@ -2,12 +2,12 @@
 
 
 const heading1 = document.querySelector(".heading1");
-const heading2 = document.querySelector(".heading");
+const heading2 = document.querySelector(".heading2");
 const heading3 = document.querySelector(".heading3");
 const heading4 = document.querySelector(".heading4");
 const heading5 = document.querySelector(".heading5");
 const heading6 = document.querySelector(".heading6");
-const heading7 = document.querySelector(".heading7");
+const heading7 = document.querySelector(".heading");
 
 
 
