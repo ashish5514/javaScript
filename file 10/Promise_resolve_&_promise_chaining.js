fe54c2bcf3893d06e5f286f95b5c7be6d422b6cf
ashish5514 <zankatashish680@gmail.com>
2023-11-03 12:53:54 +0530

    update

---
+++ file 10/Promise_resolve_&_promise_chaining.js
@@ -0,0 +1,30 @@
+ // Promise.resolve
+ // Promise chaining
+
+//  const myPromise = Promise.resolve(5);
+//  Promise.resolve(5).then(value=>{
+//     console.log(value);
+//  })
+
+// thean() method
+//   then method hamesha promise return karta hai
+
+ function myPromise(){
+    return new Promise((resolve, reject)=>{
+        resolve("foo")
+    })
+ }
+ myPromise()
+ .then(value=>{
+    console.log(value);
+    value += "bar";
+    return value;
+ })
+ .then(value=>{
+    console.log(value);
+    value+="bazz";
+    return value;
+ })
+ .then(value=>{
+    console.log(value);
+ })
